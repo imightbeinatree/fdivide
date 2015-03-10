1c698f3699600579f97c5e5fa732cfaa85f16d2d
Michael Orr <michael@cloudspace.com>
2015-03-10 11:02:32 -0400

    precision is optional, defaults to 2

--- divide_floats.go
+++ divide_floats.go
@@ -9,7 +9,10 @@ import (
 func main() {
   a, _ := strconv.ParseFloat(os.Args[1], 64)
   b, _ := strconv.ParseFloat(os.Args[2], 64)
-  prec, _ := strconv.Atoi(os.Args[3])
+  prec := 2
+  if len(os.Args) > 2 {
+   prec, _ = strconv.Atoi(os.Args[3])
+  }
   result := strconv.FormatFloat((a / b), 'f', prec, 64)
   fmt.Println("{\"result\":\""+result+"\"}")
 }
