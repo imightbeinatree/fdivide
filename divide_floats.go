eb85b808915d6e64f8ac9549484a8af3bb8edd1c
Michael Orr <michael@cloudspace.com>
2015-04-02 11:28:02 -0400

    update to handle blank string for precision

--- divide_floats.go
+++ divide_floats.go
@@ -10,7 +10,7 @@ func main() {
   a, _ := strconv.ParseFloat(os.Args[1], 64)
   b, _ := strconv.ParseFloat(os.Args[2], 64)
   prec := 2
-  if len(os.Args) > 3 {
+  if len(os.Args) > 3 && os.Args[3] != "" {
    prec, _ = strconv.Atoi(os.Args[3])
   }
   result := strconv.FormatFloat((a / b), 'f', prec, 64)
